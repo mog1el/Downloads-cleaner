31ba8d90de193a3b128e83a18130905d3f4c5911
mog1el <74179033+mog1el@users.noreply.github.com>
2024-01-23 16:40:23 +0100

    pdfs
    added moving the pdfs

--- script_windows.ps1
+++ script_windows.ps1
@@ -1,6 +1,20 @@
 $path = Get-Content -Path .\directory.txt
+New-Item -ItemType Directory -Path $path -Name "PDFs"
+New-Item -ItemType Directory -Path $path -Name "Images"
+New-Item -ItemType Directory -Path $path -Name "Rest"
 
 $fileSystemWatcher = New-Object System.IO.FileSystemWatcher
 $fileSystemWatcher.Path = $path
 $fileSystemWatcher.EnableRaisingEvents = $true
 Write-Host "hehe"
+
+$action = {
+    $changeType = $Event.SourceEventArgs.ChangeType
+    $file = $Event.SourceEventArgs.FullPath
+    Write-Host "Change detected: $changeType - $file"
+    if ($file -match '\.pdf$') {
+        $destinationFolder = $path+"PDFs"
+        Move-Item -Path $file -Destination $destinationFolder -Force
+        Write-Host "File moved to $destinationFolder"
+    }
+}
