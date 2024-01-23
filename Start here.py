6a9f808c1bfc0cf1c5819590af6714f427960fe4
mog1el <74179033+mog1el@users.noreply.github.com>
2024-01-23 18:11:04 +0100

    bash
    added bash script opener

--- Start here.py
+++ Start here.py
@@ -1,16 +1,15 @@
 import subprocess
 
 platform = input("Type 1 if you have Mac/Linux and 2 if Windows")
+print("I hope that you have PowerShell installed")
+directory = input("Write the directory that you want to keep organized: ")
+
+with open("directory.txt", "w") as file:
+    file.write(directory)
 
 if platform == 2:
     powershell_path = r'D:\Downloads-cleaner\script_windows.ps1'
     powershell_command = f'powershell -ExecutionPolicy Bypass -File "{powershell_path}"'
-
-    print("I hope that you have PowerShell installed")
-    directory = input("Write the directory that you want to keep organized: ")
-
-    with open("directory.txt", "w") as file:
-        file.write(directory)
 
     try:
         subprocess.run(powershell_command, check=True, shell=True)
@@ -19,3 +18,8 @@ if platform == 2:
         print(f"Error executing PowerShell script: {e}")
 else:
     bash_path = r'D:\Downloads-cleaner\script_else.sh'
+    try:
+        subprocess.run(["bash", bash_path], check=True)
+        print("PowerShell script executed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing PowerShell script: {e}")
