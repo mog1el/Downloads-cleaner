eb594b75f91b28a4b7e58e24eeba7a2513f23a84
mog1el <74179033+mog1el@users.noreply.github.com>
2024-01-23 18:05:46 +0100

    added choosing

--- Start here.py
+++ Start here.py
@@ -1,15 +1,21 @@
 import subprocess
-powershell_path = r'D:\Downloads-cleaner\script_windows.ps1'
-powershell_command = f'powershell -ExecutionPolicy Bypass -File "{powershell_path}"'
 
-print("I hope that you have PowerShell installed")
-directory = input("Write the directory that you want to keep organized: ")
+platform = input("Type 1 if you have Mac/Linux and 2 if Windows")
 
-with open("directory.txt", "w") as file:
-    file.write(directory)
+if platform == 2:
+    powershell_path = r'D:\Downloads-cleaner\script_windows.ps1'
+    powershell_command = f'powershell -ExecutionPolicy Bypass -File "{powershell_path}"'
 
-try:
-    subprocess.run(powershell_command, check=True, shell=True)
-    print("PowerShell script executed successfully.")
-except subprocess.CalledProcessError as e:
-    print(f"Error executing PowerShell script: {e}")
+    print("I hope that you have PowerShell installed")
+    directory = input("Write the directory that you want to keep organized: ")
+
+    with open("directory.txt", "w") as file:
+        file.write(directory)
+
+    try:
+        subprocess.run(powershell_command, check=True, shell=True)
+        print("PowerShell script executed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing PowerShell script: {e}")
+else:
+    bash_path = r'D:\Downloads-cleaner\script_else.sh'
